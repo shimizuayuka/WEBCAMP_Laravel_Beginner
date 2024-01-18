b6c6fcaa23e1002f4d59d025a85bb8d6d4d5db1d
shimizu ayuka <shimizu ayuka@example.com>
2024-01-18 01:13:17 +0000

    タスクの一覧出力を書いた

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -9,16 +9,33 @@ use App\Models\Task as TaskModel;
 
 class TaskController extends Controller
 {
-   /**
+    /**
      * タスク一覧ページ を表示する
-     *
+     * 
      * @return \Illuminate\View\View
      */
     public function list()
     {
-        return view('task.list');
+        // 一覧の取得
+        $list = TaskModel::where('user_id', Auth::id())
+                         ->orderBy('priority', 'DESC')
+                         ->orderBy('period')
+                         ->orderBy('created_at')
+                         ->get();
+/*
+$sql = TaskModel::where('user_id', Auth::id())
+                 ->orderBy('priority', 'DESC')
+                 ->orderBy('period')
+                 ->orderBy('created_at')
+                 ->toSql();
+//echo "<pre>\n"; var_dump($sql, $list); exit;
+var_dump($sql);
+*/
+        //
+        return view('task.list', ['list' => $list]);
     }
-     /**
+
+    /**
      * タスクの新規登録
      */
     public function register(TaskRegisterPostRequest $request)
