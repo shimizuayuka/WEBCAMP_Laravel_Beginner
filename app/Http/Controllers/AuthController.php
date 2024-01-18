272b0b8d41cac4f0008726dd7e0b33ffdec0e0bb
shimizu ayuka <shimizu ayuka@example.com>
2024-01-18 00:15:31 +0000

    認証と認可の実装まで書いた

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,12 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginPostRequest;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
     /**
      * トップページ を表示する
-     * 
+     *
      * @return \Illuminate\View\View
      */
     public function index()
@@ -19,7 +21,7 @@ class AuthController extends Controller
 
     /**
      * ログイン処理
-     * 
+     *
      */
     public function login(LoginPostRequest $request)
     {
@@ -27,8 +29,30 @@ class AuthController extends Controller
 
         // データの取得
         $datum = $request->validated();
+        //var_dump($datum); exit;
+
+        // 認証
+        if (Auth::attempt($datum) === false) {
+            return back()
+                   ->withInput() // 入力値の保持
+                   ->withErrors(['auth' => 'emailかパスワードに誤りがあります。',]) // エラーメッセージの出力
+                   ;
+        }
 
         //
-        var_dump($datum); exit;
+        $request->session()->regenerate();
+        return redirect()->intended('/task/list');
+    }
+
+    /**
+     * ログアウト処理
+     *
+     */
+    public function logout(Request $request)
+    {
+        Auth::logout();
+        $request->session()->regenerateToken();  // CSRFトークンの再生成
+        $request->session()->regenerate();  // セッションIDの再生成
+        return redirect(route('front.index'));
     }
 }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,29 +4,37 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateUsersTable extends Migration
 {
     /**
      * Run the migrations.
+     *
+     * @return void
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('name', 128);
+            $table->string('email', 254)->unique();
+            $table->datetime('email_verified_at')->nullable();
+            $table->string('password', 255);
             $table->rememberToken();
-            $table->timestamps();
+            //$table->timestamps();
+            $table->dateTime('created_at')->useCurrent();
+            $table->dateTime('updated_at')->useCurrent()->useCurrentOnUpdate();
+            //
+            $table->collation = 'utf8mb4_bin';
         });
     }
 
     /**
      * Reverse the migrations.
+     *
+     * @return void
      */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('users');
     }
-};
+}
